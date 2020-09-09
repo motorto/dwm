0451c102371a4b3e502e41c7db7009ce70299aa9
André Cerqueira <acerqueira021@gmail.com>
2020-09-09 17:50:15 +0100

    add zoom rule

--- config.def.h
+++ config.def.h
@@ -33,6 +33,7 @@ static const Rule rules[] = {
 	{ "Firefox","Toolkit","Picture-in-Picture",1 << 1,       1,           -1 },
 	{ NULL , NULL,				"LibreOffice",				1<<2 ,			0,							-1},
 	{ NULL , "libreoffice",	NULL,				1<<2 ,			0,							-1},
+	{ "zoom", "zoom",	NULL,				1<<3 ,			1,							-1},
 	{ "XTerm",  "xterm",     "neomutt",       1 << 3,       0,           -1 },
 };
 
